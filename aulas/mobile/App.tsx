f3f16b2476749e687b99e6830ab8ba25108cde33
jgapadua <joaogabrielpadua@gmail.com>
2023-02-23 17:38:54 -0300

    status bar custom

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -27,7 +27,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </View>
   );
 }
